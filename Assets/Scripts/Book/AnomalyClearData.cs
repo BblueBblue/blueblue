fb4de7afaf07cd78cbb1ab5fccfad6fc9622298e
owl2lwo12 <owl2lwo12@ajou.ac.kr>
2025-03-30 20:23:22 +0900

    Feat: achievement ++
    - 모든 이상현상 발견시 작동하는 도전과제

--- Assets/Scripts/Book/AnomalyClearData.cs
+++ Assets/Scripts/Book/AnomalyClearData.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Steamworks;
+using Steamworks.Data;
 
 public class AnomalyClearData
 {
     public Dictionary<string, bool> anomalyClearDictionary; // 이상현상 이름, 클리어 여부.
 
+    public int clearCount = 0;
+
     public void AddData(string anomalyName)
     {
-        anomalyClearDictionary.Add(anomalyName, false);    
+        anomalyClearDictionary.Add(anomalyName, false);  
+        clearCount++;
     }
     
     public bool TryCheckData(string anomalyName)
@@ -16,6 +21,15 @@ public class AnomalyClearData
         if (anomalyClearDictionary.ContainsKey(anomalyName))
         {
             anomalyClearDictionary[anomalyName] = true;
+            clearCount--;
+
+            if(clearCount == 0 && SteamClient.IsValid)
+            {
+                Achievement ach = new Achievement("DIARYCLEAR");
+
+                if (!ach.State)
+                    ach.Trigger();
+            }
             return true;
         }
 
